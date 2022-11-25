a7d80ebbc8f7f672bc92282ae5bf154895a5898b
tefa2611 <tefa261105@gmail.com>
2022-11-24 22:23:32 -0500

    Contacto

---
+++ form.js
@@ -0,0 +1,14 @@
+document.querySelector('#submit').addEventListener('click',function(){
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573227038265";
+
+    let nombre = document.querySelector("#nombre").value;
+    let mail = document.querySelector("#mail").value;
+    let titulo = document.querySelector("#titulo").value;
+    let texto = document.querySelector("#texto").value;
+
+    let url = "https://api.whatsapp.com/send?phone=573227038265&text= Hola Comida Japonesa%0A Soy " + nombre + "%0A Mi E-mail es: " + mail + "%0A Quiero hablarles sobre: " + titulo + "%0A Mi mensaje es: " + texto;
+
+    window.open(url);
+});
